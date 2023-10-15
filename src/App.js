859de21d494510284457e4374d74acd198e71cc4
aditya <adiraiar022@gmail.com>
2023-10-15 11:11:22 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import LobbyScreen from './screens/Lobby';
+import RoomPage from './screens/Room';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<LobbyScreen />} />
+          <Route path='/room/:roomId' element = {<RoomPage/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
